5cf8a54c750ee61b1572da91d10a5a5a01c83b8a
maksymfedosov <ctpahhuk22@gmail.com>
2022-06-24 22:52:51 +0300

    fixed small thngs

--- src/components/day/Day.js
+++ src/components/day/Day.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect, useCallback } from "react";
-import classes from "./Day.module.css";
-import TodoList from "./todo/TodoList";
-import AddTodo from "./addtodo/AddTodo";
+import React, { useState, useEffect, useCallback } from 'react'
+import classes from './Day.module.css'
+import TodoList from './todo/TodoList'
+import AddTodo from './addtodo/AddTodo'
 
 const DUMMY_TODO = [
   {
-    id: "1",
-    text: "Do something",
-    hardness: "easy",
-    scarity: "scary",
+    id: '1',
+    text: 'Do something',
+    hardness: 'easy',
+    scarity: 'scary',
     ready: false,
   },
   {
-    id: "2",
-    text: "Buy something",
-    hardness: "hard",
-    scarity: "scary",
+    id: '2',
+    text: 'Buy something',
+    hardness: 'hard',
+    scarity: 'scary',
     isReady: false,
   },
-];
+]
 
 const Day = () => {
-  const [todoList, setTodoList] = useState([]);
-  const [error, setError] = useState(null);
+  const [todoList, setTodoList] = useState([])
+  const [error, setError] = useState(null)
 
   // async function fetchMoviesHandler() {
   const fetchMoviesHandler = useCallback(async () => {
     // setIsLoading(true);
-    setError(null);
+    setError(null)
     try {
       const response = await fetch(
-        "https://todo-6d56a-default-rtdb.europe-west1.firebasedatabase.app/todos.json"
-      );
+        'https://todo-6d56a-default-rtdb.europe-west1.firebasedatabase.app/todos.json',
+      )
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        throw new Error('Something went wrong!')
       }
 
-      const data = await response.json();
+      const data = await response.json()
 
       setTodoList(data);
 
@@ -51,17 +51,17 @@ const Day = () => {
       //   });
       // }
     } catch (error) {
-      setError(error.message);
+      setError(error.message)
     }
     // setIsLoading(false);
-  }, []);
+  }, [])
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+  // useEffect(() => {
+  //   fetchMoviesHandler()
+  // }, [])
 
   const onAddTodoHandler = (props) => {
-    // console.log("onAddTodoHandler")
+    // console.log('onAddTodoHandler')
     setTodoList((prevTodoList) => {
       return [
         ...prevTodoList,
@@ -72,15 +72,16 @@ const Day = () => {
           isReady: false,
           id: Math.random().toString(),
         },
-      ];
-    });
+      ]
+    })
+  }
 
-    return (
-      <div className={classes.day}>
-        <AddTodo onAddTodo={onAddTodoHandler} />
-        <TodoList todos={todoList} />
-      </div>
-    );
-  };
-};
-export default Day;
+  return (
+    <div className={classes.day}>
+      <AddTodo onAddTodo={onAddTodoHandler} />
+      <TodoList todos={todoList} />
+    </div>
+  )
+}
+
+export default Day
