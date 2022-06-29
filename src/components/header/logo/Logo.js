7ff0f36a18ecb02c680f83516f3ee732c942baff
maksym.fedosov@tesco.com <maksym.fedosov@tesco.com>
2022-06-29 21:11:28 +0300

    added logo

--- src/components/header/logo/Logo.js
+++ src/components/header/logo/Logo.js
@@ -1,7 +1,14 @@
 import classes from "./Logo.module.css";
+// import logoImage from "../../assets/shield.png";
+import logoImage from "../../../assets/shield.png";
 
 const Logo = () => {
-  return <p className={classes.logo}>LOGO</p>;
+  // return <p className={classes.logo}>LOGO</p>;
+  return (
+    <div className={classes[classes.logo]}>
+      <img src={logoImage} alt="Logo" />
+    </div>
+  );
 };
 
 export default Logo;
